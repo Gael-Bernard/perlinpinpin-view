88c3efcf32688850d65da19e8f037f18fc81b253
Gael-Bernard <job.gael.bernard@gmail.com>
2023-07-23 23:47:20 +0200

    feature: Added transition between both biomes in simple biomes

--- src/app/world-models/square-world/simple-biomes/SimpleBiomes2DGenerator.ts
+++ src/app/world-models/square-world/simple-biomes/SimpleBiomes2DGenerator.ts
@@ -4,6 +4,7 @@ import MultilayerPerlin from "src/app/generation/noise-functions/perlin/Multilay
 import UnicolorBlock2D from './UnicolorBlock2D';
 import Color from "src/app/base-components/color/Color";
 import Perlin from "src/app/generation/noise-functions/perlin/Perlin";
+import LinearInterpolation from "src/app/maths/interpolation/LinearInterpolation";
 
 export default class SimpleBiome2DGenerator {
 
@@ -12,7 +13,7 @@ export default class SimpleBiome2DGenerator {
   public oceanColor: Color = Color.BLUE;
   public minGroundAltitude: number = 24;
   public minOceanAltitude: number = 4;
-  public biomeNoiseAmplitude: number = 64;
+  public biomeNoiseAmplitude: number = 256;
   public seaLevel: number = 20;
 
   private landscape!: MultilayerPerlin;
@@ -52,14 +53,24 @@ export default class SimpleBiome2DGenerator {
   get(position: Vec2): SimpleBiome2DBlock {
     const value01 = (this.landscape.at( new Vec2(Math.floor(position.x), 0.3) ) + 1) / 2;
     const biomeVal = (this.biomeNoise.at( new Vec2(position.x / this.biomeNoiseAmplitude, 0.3) ) + 1) / 2;
-    if(biomeVal < 0.5) { // land
+    if(biomeVal < 0.45) { // land
       return new UnicolorBlock2D( position.y > value01 * 16 + this.minGroundAltitude ? this.skyColor : this.groundColor);
     }
-    else { // ocean
+    else if(biomeVal > 0.55) { // ocean
       if(position.y <= value01 * 16 + this.minOceanAltitude) {
         return new UnicolorBlock2D(this.groundColor);
       }
       return new UnicolorBlock2D( position.y > this.seaLevel ? this.skyColor : this.oceanColor);
+    }
+    else { // transition ocean/land
+      const oceanity = (biomeVal - 0.45) * 10;
+      const ySkyline = LinearInterpolation.applyForDx1(value01 * 16 + this.minGroundAltitude, value01 * 16 + this.minOceanAltitude, oceanity);
+      if(position.y <= ySkyline) {
+        return new UnicolorBlock2D(this.groundColor);
+      }
+      else {
+        return new UnicolorBlock2D( position.y > this.seaLevel ? this.skyColor : this.oceanColor);
+      }
     }
   }
 
